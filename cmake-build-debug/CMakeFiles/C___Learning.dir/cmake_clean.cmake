8f335d708ca2040373b294d677dab42ed140b8e6
Timothy Wu <timothywu02@gmail.com>
2020-11-22 19:41:59 -0500

    First time with C++

---
+++ cmake-build-debug/CMakeFiles/C___Learning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Learning.dir/main.cpp.obj"
+  "C___Learning.exe"
+  "C___Learning.exe.manifest"
+  "C___Learning.pdb"
+  "libC___Learning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << 15 << endl;
+    cout << sizeof(5) << endl;
+    int x;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Your number is " << x;
+    return 0;
+}
